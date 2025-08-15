dcb6f879551ba9fb64e19d8523ba6e6afbfd4531
Francisco Barrento <francisco.barrento@gmail.com>
2025-08-15 20:31:04 +0100

    refactor: removing unused files

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use TenantForge\Filament\Pages\Auth\Register;
 
-Route::get('/', function () {
-    return 'Hello World!';
-});
-
 Route::get('/sign-up', Register::class)
     ->name('tenantforge.sign-up');
